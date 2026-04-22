e3e501b878ce904320bdb14e7fcfd56b3f26c123
Mikael Souza <mikael.souza.cc@gmail.com>
2026-04-21 22:56:08 -0300

    Add git and distrobox into the package selection

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    distrobox
+    git
     vim  
   ];
 
