5bb72ef88a324bad41e835b0024fa340796ab41e
Mikael Souza <mikaelsouza@hrist.local>
2026-05-03 18:41:21 -0300

    Use r8168 driver instead of r8169 and activate aspm=1 and eee=1.

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.extraModulePackages = [ config.boot.kernelPackages.r8168 ];
+  boot.blacklistedKernelModules = [ "r8169" ];
+  boot.kernelModules = [ "r8168" ];
+  boot.extraModprobeConfig = "options r8168 aspm=1 eee=1";
+
   networking.hostName = "silmeria"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
